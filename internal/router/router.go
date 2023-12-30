cb7f4df8a17685e0468e73f35f2091c23adc8136
Robert Bublik <robert.bublik@de.rhenus.com>
2023-12-31 00:39:46 +0100

    Remove unused routes

--- internal/router/router.go
+++ internal/router/router.go
@@ -6,9 +6,6 @@ import (
 	"github.com/bobertrublik/webhook-router/internal/logger"
 	"net/http"
 
-	"github.com/auth0/go-jwt-middleware/v2"
-	"github.com/auth0/go-jwt-middleware/v2/validator"
-
 	"github.com/bobertrublik/webhook-router/internal/middleware"
 )
 
@@ -31,45 +28,6 @@ func New(webhookDaemon *daemon.WebhookDaemon) *http.ServeMux {
 			if err != nil {
 				fmt.Println(err)
 			}
-		}),
-	))
-
-	// This route is only accessible if the user has a valid access_token.
-	router.Handle("/api/private", middleware.EnsureValidToken()(
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// CORS Headers.
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
-			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-			w.Header().Set("Access-Control-Allow-Headers", "Authorization")
-
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(`{"message":"Hello from a private endpoint! You need to be authenticated to see this."}`))
-		}),
-	))
-
-	// This route is only accessible if the user has a
-	// valid access_token with the read:messages scope.
-	router.Handle("/api/private-scoped", middleware.EnsureValidToken()(
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// CORS Headers.
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
-			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-			w.Header().Set("Access-Control-Allow-Headers", "Authorization")
-
-			w.Header().Set("Content-Type", "application/json")
-
-			token := r.Context().Value(jwtmiddleware.ContextKey{}).(*validator.ValidatedClaims)
-
-			claims := token.CustomClaims.(*middleware.CustomClaims)
-			if !claims.HasScope("read:messages") {
-				w.WriteHeader(http.StatusForbidden)
-				w.Write([]byte(`{"message":"Insufficient scope."}`))
-				return
-			}
-
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(`{"message":"Hello from a private endpoint! You need to be authenticated to see this."}`))
 		}),
 	))
 
